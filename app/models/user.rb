2a23b406b620c3a2dd0b75a568fafd4f0cc61a8c
sakai <m0a4x02@gmail.com>
2019-10-28 12:45:17 +0000

    修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,7 @@ class User < ApplicationRecord
   validates :name, length: {maximum: 15}
   validates :email, presence: true
   validates :email,format: /\A[\w\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :password,length: {minimum: 8, maximum: 32}
-  validates :password,format: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,100}+\z/i
+  validates :password,format: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}+\z/i
   
   has_secure_password
 end
