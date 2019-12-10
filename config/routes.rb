d2dbb2208e1e8066c64fd78db86d447a3831a61c
sakai <m0a4x02@gmail.com>
2019-12-10 09:07:02 +0000

    課題7

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,10 @@ get 'sessions/new'
 
   resources :users
   resources :topics
+  
+  get 'favorites/index'
+  post '/favorites', to: 'favorites#create'
+  delete '/favorites', to: 'favorites#destroy'
+  
  
 end
